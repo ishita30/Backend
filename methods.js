1a1530a5116fbe8adfc7b24b45e1a316c3005b07
ishita30 <ishitasinghal30@gmail.com>
2022-03-29 15:39:11 +0530

    Update method.js

--- methods.js
+++ methods.js
@@ -5,6 +5,10 @@ let users = {}
 app.get('/users', (req, res) => {
   res.send(users)
 })
+app.post()
 //get
 
 //post->to send data from front end to backend
+app.post('/users', (req, res) => {
+  console.log(req.body)
+})
