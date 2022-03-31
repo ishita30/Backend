d5e499bd129388c3527ba47e2c4af6e47a745e59
ishita30 <ishitasinghal30@gmail.com>
2022-03-31 23:42:45 +0530

    Update method.js

--- methods.js
+++ methods.js
@@ -5,18 +5,41 @@ const app = express()
 app.use(express.json())
 app.listen(3000)
 let users = {}
-app.get('/users', (req, res) => {
+app.get('/user', (req, res) => {
   res.send(users)
 })
 
 //get
 
 //post->to send data from front end to backend
-app.post('/users', (req, res) => {
+app.post('/user', (req, res) => {
   console.log(req.body)
   users = req.body
-  res.send({
+  res.json({
     message: 'data receive successfully',
     user: req.body,
   })
 })
+
+//update->patch
+app.patch('/user', (req, res) => {
+  console.log('req.body->', req, res)
+  //update data in users obj
+  let dataToBeUpdated = req.body
+  for (key in req.body) {
+    users[key] = dataToBeUpdated[key]
+  }
+  res.json({
+    message: 'data updated successfully',
+  })
+})
+
+//to delete the data
+app.delete('/user',(req,res)=>
+{
+  users={};
+  res.json({
+    message: "data has been deleted";
+  })
+}
+)
