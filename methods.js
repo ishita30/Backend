ae05820b0d36c5e53297dba3014a63c528a14e2d
ishita30 <ishitasinghal30@gmail.com>
2022-03-31 21:46:50 +0530

    Update method.js

--- methods.js
+++ methods.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const app = express()
+
+//middleware function, post, front->json
+app.use(express.json())
 app.listen(3000)
 let users = {}
 app.get('/users', (req, res) => {
   res.send(users)
 })
-app.post()
+
 //get
 
 //post->to send data from front end to backend
 app.post('/users', (req, res) => {
   console.log(req.body)
+  users = req.body.Name
+  res.send({
+    message: 'data receive successfully',
+    user: req.body,
+  })
 })
